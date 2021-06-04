cc272724c6bcafd467505ce11004970d536232f5
poppymills2@gmail.com <poppymills2@gmail.com>
2021-06-03 20:12:08 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "889845155077440",
+    clientSecret: "8ac75f66bd2ef0a18839582d32b1ac5a",
+  },
 };
